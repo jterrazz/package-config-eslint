ef99207f9fcd7f9ce7eac17e1dbbfdb54a38d8f9
Jean-Baptiste Terrazzoni <j2aa@proton.me>
2023-06-25 12:01:04 +0200

    [CHORE] Separate front linter from this common package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
     extends: [
-        'next/core-web-vitals',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
         'plugin:promise/recommended',
